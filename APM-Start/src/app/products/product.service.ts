5b7d2d221d4946d59d70e76c1cc225b724ce495e
m.kikmunter <M.Kikmunter@gmail.com>
2019-08-19 21:18:09 +0100

    improved hadleError - didn't like original

--- APM-Start/src/app/products/product.service.ts
+++ APM-Start/src/app/products/product.service.ts
@@ -14,7 +14,7 @@ export class ProductService {
   private productsUrl = 'api/products';
 
   constructor(private http: HttpClient,
-              private supplierService: SupplierService) { }
+    private supplierService: SupplierService) { }
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
@@ -38,17 +38,10 @@ export class ProductService {
   }
 
   private handleError(err: any) {
-    // in a real world app, we may send the server to some remote logging infrastructure
-    // instead of just logging it to the console
-    let errorMessage: string;
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `Backend returned code ${err.status}: ${err.body.error}`;
-    }
+    const errorMessage: string = err.error instanceof ErrorEvent
+      ? `An error occurred: ${err.error.message}`
+      : `Backend returned code ${err.status}: ${err.body.error}`;
+
     console.error(err);
     return throwError(errorMessage);
   }
